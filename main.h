d5b9dc176831bfa54b6e1c76b7e18d6c7d922929
EMarvelM <marvelousegbe19@gmail.com>
2023-07-25 20:04:20 +0000

    created function for util and the header file

--- main.h
+++ main.h
@@ -1,11 +1,55 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 
-void _putchar(char c);
-int _printf(const char *format, ...);
-int _putstr(char **str);
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
-#endif /* MAIN_H */
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
